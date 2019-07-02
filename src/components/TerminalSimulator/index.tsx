d7e855492afad7fb308eda4b3f9a485260464f90
Peter Pang <mv@live.cn>
2019-07-02 15:36:37 +0800

    Refine terminal.

--- src/components/TerminalSimulator/index.tsx
+++ src/components/TerminalSimulator/index.tsx
@@ -5,21 +5,15 @@ import './style.scss';
 export interface ITerminalSimulatorProps {
   executeCommand: any;
   msg: string;
+  promptSymbol: string;
 }
 
 export default class TerminalSimulator extends React.Component<ITerminalSimulatorProps, any> {
   public render() {
     return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh"
-        }}
-      >
-        <Terminal msg={this.props.msg} watchConsoleLogging={true}
-          commandPassThrough={(cmd, print) => this.props.executeCommand(cmd, print) } 
+      <div>
+        <Terminal msg={this.props.msg} watchConsoleLogging={true} hideTopBar={true} showActions={false}
+          promptSymbol={this.props.promptSymbol} commandPassThrough={(cmd, print) => this.props.executeCommand(cmd, print)}
         />
       </div>
     );
--- src/containers/TerminalPage.tsx
+++ src/containers/TerminalPage.tsx
@@ -8,11 +8,11 @@ export interface ITerminalPageProps {
 }
 
 class TerminalPage extends React.Component<ITerminalPageProps, any> {
-  socket: any;
-  componentDidMount()
-  {
+  constructor(props) {
+    super(props);
+    this.state = { promptSymbol: null };
     var websocketUrlCompiled = _.template('wss://${host}:${port}/terminal/api/v1/namespaces/${namespace}/pods/${pod}/exec?command=/bin/bash&stdin=true&stderr=true&stdout=true&tty=true&container=${container}');
-    var websocketUrl = websocketUrlCompiled({ 
+    var websocketUrl = websocketUrlCompiled({
       'host': this.props.match.params.host,
       'port': this.props.match.params.port,
       'namespace': this.props.match.params.namespace,
@@ -24,35 +24,69 @@ class TerminalPage extends React.Component<ITerminalPageProps, any> {
     this.socket.onclose = this.close;
     this.socket.onopen = this.open;
   }
+  socket: any;
+  componentDidMount() {
+    
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.promptSymbol===null) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
   @autobind
   open() {
-    console.log('Socket open.');
-    //this.socket.send('{"Width":80,"Height":24}');
+    console.log('Terminal open.');
   }
   @autobind
   close() {
-    console.log('Socket close.');
+    console.log('Terminal close.');
   }
   @autobind
   handleMessage(message) {
-    if(typeof(message.data)==='string')
-    {
-      console.log(message.data);
+    if (typeof (message.data) === 'string' && message.data != '') {
+      var messageSegments = _.split(message.data, '\r\n');
+      if (messageSegments.length === 1) {
+        if (_.endsWith(messageSegments[0], '# ')) {
+          this.setState({ promptSymbol: _.trim(messageSegments[0]) });
+        }
+        else { }
+      }
+      else if (messageSegments.length > 1) {
+        messageSegments.map((messageSegment, index) => {
+          if (index != 0 && messageSegment != '' && !_.endsWith(messageSegment, '# ')) {
+            console.log(messageSegment);
+          }
+        });
+      }
+      else {
+        console.log('Not implement');
+      }
     }
-    else if(typeof(message.data)==='object')
-    {
+    else if (typeof (message.data) === 'object') {
       console.log(message.data);
     }
   }
   @autobind
   executeCommand(cmd, print) {
-    this.socket.send(cmd);
+    var message = '';
+    cmd.map((item) => {
+      message += ' ' + item;
+    });
+    this.socket.send(message);
     //print('Hi');
   }
   render() {
+    let terminalSimulator;
+    if(this.state.promptSymbol)
+    {
+      terminalSimulator = <TerminalSimulator promptSymbol={this.state.promptSymbol} msg='Wellcome to FewBox' executeCommand={this.executeCommand} />;
+    }
     return (
       <div>
-        <TerminalSimulator msg='Wellcome to FewBox' executeCommand={this.executeCommand} />
+        {terminalSimulator}
       </div>
     );
   }
