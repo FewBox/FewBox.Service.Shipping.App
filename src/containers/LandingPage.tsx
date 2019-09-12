82a05a922488ccce1f6425c0a279fac0c1787af7
Peter Pang <mv@live.cn>
2019-09-12 18:58:30 +0800

    Refine code.

--- src/containers/LandingPage.tsx
+++ src/containers/LandingPage.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { Store, ShippingIndustryStatus } from '../reducers/State';
 import { initLandingPage } from '../actions';
-import { Row, Col, Statistic, Button, List, Card, Descriptions } from 'antd';
+import { Row, Col, Statistic, Button, List, Card, Descriptions, Badge } from 'antd';
 
 export interface ILandingPageProps {
   initLandingPage: any;
@@ -17,13 +17,13 @@ class LandingPage extends React.Component<ILandingPageProps, any> {
     return (
       <div>
         <Row gutter={16}>
-          <List grid={{ gutter: 16, column: 3 }} dataSource={this.props.shippingIndustryStatuses}
+          <List grid={{ gutter: 16, column: 4 }} dataSource={this.props.shippingIndustryStatuses}
             renderItem={(item: ShippingIndustryStatus) => (
               <List.Item>
                 <Card>
                   <p>{item.name}</p>
                   {item.conditions.map((condition, index) => {
-                    return <p><p>{condition.type}</p><p>{condition.status}</p><p>{condition.message}</p></p>
+                    return <p><Badge color={condition.status === 'True' ? 'green' : 'red'} text={condition.status} /></p>
                   })}
                 </Card>
               </List.Item>
