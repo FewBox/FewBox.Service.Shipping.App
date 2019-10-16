b682dd964c932d756441e704ee8661ba498deeed
Peter Pang <mv@live.cn>
2019-10-16 15:22:36 +0800

    Change SessionAffinityOptions to sessionAffinityOptions.

--- src/jsons/index.ts
+++ src/jsons/index.ts
@@ -3,7 +3,7 @@ import * as LocationOptions from './locationOptions.json';
 import * as ResolutionOptions from './resolutionOptions.json';
 import * as MatchOptions from './matchOptions.json';
 import * as ServiceOptions from './serviceOptions.json';
-import * as SessionAffinityOptions from './SessionAffinityOptions.json';
+import * as SessionAffinityOptions from './sessionAffinityOptions.json';
 import * as ImagePullPolicyOptions from './imagePullPolicyOptions.json';
 import * as TLSModeOptions from './tlsModeOptions.json';
 
